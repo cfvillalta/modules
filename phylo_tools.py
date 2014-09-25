649f65831027d6b2efedc8622e8d95b20c9780a9
Chris VIllalta <cfvillalta@gmail.com>
2014-09-25 16:33:44 -0700

    made both functions in the phylotools module they should work now.

--- phylo_tools.py
+++ phylo_tools.py
@@ -3,3 +3,29 @@
 #this will be a module with my phylogenetic analysis tools. e.g. fasttree tree building, clustalo.
 
 
+#first function will be to run clustalo from python.
+
+
+
+
+
+#Run fasttreeMP outputs newick file.
+def ClustalO(file_main_name):
+    print 'Begin clustalo'
+    clustalo = Popen(['time', 'clustalo', '-i', '%s_unaligned.fa' %(file_main_name), '-o', '%s_aligned_clustalo.fa' %(file_main_name), '--force'])
+    clustalo.communicate()
+    
+    print 'Done with clustalo'
+
+
+#want to run in the background but have an issue piping when nohup
+def  FastTreeMP(file_main_name):
+    import sys
+    print "Begin FastTreeMP"
+    FastTreeMP = Popen(['FastTreeMP', '-quiet', '-nopr', '-log', '%s_fasttree.log' %(file_main_name), '%s_aligned_clustalo.fa' %(file_main_name)],stdout=PIPE,shell=False)
+    newick_out = open("%s_clustalo_fasttree.newick" %(file_main_name), 'w')
+    newick_out.write(FastTreeMP.stdout.read())
+
+    FastTreeMP.communicate()
+    newick_out.close()
+    print "Done with FastTreeMP"
