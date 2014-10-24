518f103875f6fc28ef5ff86704d9bdd4f9f238c0
Chris VIllalta <cfvillalta@gmail.com>
2014-10-24 15:41:30 -0700

    added function GID2seq to module

--- phylo_tools.py
+++ phylo_tools.py
@@ -42,3 +42,39 @@ def fasta2cdt(file_main_name):
     fasta2cdt = Popen(['fasta2cdt.py', file_main_name],stdout=PIPE, shell=False)
     print 'Done with conversion'
 
+
+#input list of GIDs and output a dictionary with GID key and list value [species name,sequence]
+
+def GID2seq(list_name_here):
+    from subprocess import Popen, PIPE
+    fasta_seqs = {}
+    for GID in list_name_here:
+        p = Popen(['blastdbcmd', '-db', 'nr', '-dbtype', 'prot', '-entry', GID, '-target_only','-outfmt', '%t\t%s'],stdout = PIPE)
+        stdout = p.stdout.read()
+        p.communicate()
+        if stdout:
+            stdout_split=stdout.split("\t")
+            ID = stdout_split[0].strip()
+            ID = ID.replace(' ', '_')  
+            ID = ID.replace('[', '')
+            ID = ID.replace(']', '')
+            ID = ID.replace(',', '_')
+            ID = ID.replace('/', '_')
+            ID = ID.replace('__', '_')
+            ID = ID.replace(':', '_')
+            ID = ID.replace('.', '_')
+            ID = ID.replace('(', '_')
+            ID = ID.replace(')', '_')
+            ID = ID.replace(';', '_')
+            ID = ID.replace('+', '_')
+            ID = ID.replace('=', '_')
+            ID = ID.replace('__', '_')
+            ID = ID.replace('__', '_')
+            ID = ID.replace('__', '_')
+       
+            fasta_seqs[GID]=[ID,stdout_split[1]]
+            #print stdout_split
+        else:
+            print "no GID sequecne %s" %(GID)
+
+    return fasta_seqs
